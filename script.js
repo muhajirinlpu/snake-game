2944ad291288117504f5777d6d4e9d797a22d2bd
Muhajirin <muhajirinlpu@gmail.com>
2020-05-07 19:06:28 +0700

    fix bug body spreading causes tail confused with reverse direction with head

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const PRIMARY_COLOR = '#fad2e3',
-  WIDTH = 700,
-  HEIGHT = 700,
-  SPEED = 5
+const PRIMARY_COLOR = '#fad2e3', SECONDARY_COLOR = '#a61d24'
+  WIDTH = 700, HEIGHT = 700,
+  SPEED = 5,
+  DIRECTION_UP = 'up', DIRECTION_DOWN = 'down', DIRECTION_LEFT = 'left', DIRECTION_RIGHT = 'right'
 
 const canvas = document.getElementById('game'),
   context = canvas.getContext('2d')
 
 const state = {
-  direction: null
+  direction: DIRECTION_RIGHT
 }
 
 const createBodyPart = ({ x = 0 , y = 0, w = 10, h = 10, direction }) => ({
@@ -28,22 +28,13 @@ class Game {
   }
 
   compute () {
-    // move bodyPart
-    for (let i = 0; i < this.body.length; i++) {
-      const bodyPart = this.body[i]
+    // move bodyPart through their direction
+    for (const bodyPart of this.body) {
       switch (bodyPart.direction) {
-        case 'up':
-          bodyPart.y -= SPEED
-          break
-        case 'down':
-          bodyPart.y += SPEED
-          break
-        case 'left':
-          bodyPart.x -= SPEED
-          break
-        case 'right':
-          bodyPart.x += SPEED
-          break
+        case DIRECTION_UP: bodyPart.y -= SPEED; break
+        case DIRECTION_DOWN: bodyPart.y += SPEED; break
+        case DIRECTION_LEFT: bodyPart.x -= SPEED; break
+        case DIRECTION_RIGHT: bodyPart.x += SPEED; break
       }
     }
     // direction process
@@ -51,7 +42,6 @@ class Game {
       const bodyPart = this.body[i]
       if (i === 0 && state.direction !== null) {
         bodyPart.direction = state.direction
-        state.direction = null
       } else if (i !== 0 && bodyPart.direction !== this.body[i - 1].direction &&
         (this.body[i - 1].x === bodyPart.x || this.body[i - 1].y === bodyPart.y)) {
         bodyPart.direction = this.body[i - 1].direction
@@ -63,9 +53,10 @@ class Game {
     context.clearRect(0, 0, WIDTH, HEIGHT)
 
     // draw snakes
+    context.fillStyle = SECONDARY_COLOR
     for (const { x, y, w, h } of this.body) {
-      context.fillStyle = PRIMARY_COLOR
       context.fillRect(x, y, w, h)
+      context.fillStyle = PRIMARY_COLOR
     }
   }
 
@@ -83,14 +74,14 @@ class Game {
 const game = new Game()
 
 window.onkeydown = e => {
-  if (e.code === 'ArrowUp' || e.code === 'KeyW') {
-    state.direction = 'up'
-  } else if (e.code === 'ArrowDown' || e.code === 'KeyS') {
-    state.direction = 'down'
-  } else if (e.code === 'ArrowLeft' || e.code === 'KeyA') {
-    state.direction = 'left'
-  } else if (e.code === 'ArrowRight' || e.code === 'KeyD') {
-    state.direction = 'right'
+  if ((e.code === 'ArrowUp' || e.code === 'KeyW') && state.direction !== DIRECTION_DOWN) {
+    state.direction = DIRECTION_UP
+  } else if ((e.code === 'ArrowDown' || e.code === 'KeyS') && state.direction !== DIRECTION_UP) {
+    state.direction = DIRECTION_DOWN
+  } else if ((e.code === 'ArrowLeft' || e.code === 'KeyA') && state.direction !== DIRECTION_RIGHT) {
+    state.direction = DIRECTION_LEFT
+  } else if ((e.code === 'ArrowRight' || e.code === 'KeyD') && state.direction !== DIRECTION_LEFT) {
+    state.direction = DIRECTION_RIGHT
   }
 }
 
